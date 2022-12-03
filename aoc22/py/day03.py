33c54a30fadbc195ef950932095e2bef1bb6ef8b
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-03 07:14:45 +0200

    day03.py/2

--- aoc22/py/day03.py
+++ aoc22/py/day03.py
@@ -5,6 +5,15 @@ import string
 from utils import inputs, examples
 
 
+def score(item):
+    if item in string.ascii_lowercase:
+        score = ord(item) - ord('a') + 1
+    else:
+        score = ord(item) - ord('A') + 27
+
+    return score
+
+
 def day03(filename):
     print()
     print(filename)
@@ -12,6 +21,8 @@ def day03(filename):
     with open(filename) as puzzlein:
         plays = []
         total = 0
+        group = []
+        badges = 0
         for line in puzzlein:
             line = line.strip()
             #print(len(line), len(line)/2)
@@ -19,15 +30,18 @@ def day03(filename):
             common = set(left).intersection(set(right))
             assert len(common) == 1
             item = common.pop()
-            if item in string.ascii_lowercase:
-                score = ord(item) - ord('a') + 1
-            else:
-                score = ord(item) - ord('A') + 27
+            total += score(item)
 
-            print(item, score)
-            total += score
+            group.append(set(line))
+            if len(group) == 3:
+                badge = group[0].intersection(group[1]).intersection(group[2]).pop()
+                badges += score(badge)
+                print(badge, badges)
+                group = []
+
 
         print(total)
+        print(badges)
 
 
 
