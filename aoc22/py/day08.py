672d7471d5d5bcb6b041db1d0fd59f31d1c8b6ce
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-08 11:32:30 +0200

    No edges

--- aoc22/py/day08.py
+++ aoc22/py/day08.py
@@ -13,15 +13,13 @@ def day08(filename):
         # TODO shaping
         data = pd.DataFrame(data=[list(map(int, line.strip())) for line in puzzlein])
 
-        visible_trees = set()
+        visible_interior_trees = set()
         found = data.copy()
 
-        # TODO: avoid the edges since they are trivial
         # TODO: combine part1 and part2 by viewing from outside
-
         max_scenic = 0
-        for rowix in range(data.shape[0]):
-            for colix in range(data.shape[0]):
+        for rowix in range(1, data.shape[0] - 1):
+            for colix in range(1, data.shape[0] - 1):
                 tree = data.loc[rowix][colix]
                 from_left = data.loc[rowix][:colix][::-1]
                 from_right = data.loc[rowix][colix+1:]
@@ -31,8 +29,8 @@ def day08(filename):
                 if (from_left.max() < tree or 
                     from_right.max() < tree or
                     from_top.max() < tree or
-                    from_bottom.max() < tree or rowix == 0 or colix == 0 or rowix == data.shape[0] - 1 or colix == data.shape[1] - 1):
-                        visible_trees.add((rowix, colix))
+                    from_bottom.max() < tree):
+                        visible_interior_trees.add((rowix, colix))
                         found.loc[(rowix, colix)] = -100
 
                 scenic = 1
@@ -47,9 +45,10 @@ def day08(filename):
                 if scenic > max_scenic:
                     max_scenic = scenic
 
-
-        print("part1 visible trees from outside:", len(visible_trees))
+        outer_visible_trees = 2 * data.shape[0] + 2 * data.shape[1] - 4
+        print("part1 visible trees from outside:",  outer_visible_trees + len(visible_interior_trees))
         print("part2 max scenic view:", max_scenic)
+
 
 day08(inputs("08"))
 day08(examples("08"))
