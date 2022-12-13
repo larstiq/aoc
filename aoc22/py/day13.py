c6c972b6868016c60ca3e7378d487829cbe2932b
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-13 07:18:36 +0200

    day13.py/1

---
+++ aoc22/py/day13.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+from utils import inputs, examples
+
+import collections
+import copy
+import logging
+import math
+
+import sympy
+
+import pandas as pd
+
+import numpy as np
+import string
+import networkx
+import itertools
+
+
+def cmp_nested_list(left, right):
+    for (l, r) in zip(left, right):
+        if isinstance(l, int) and isinstance(r, int):
+            if l == r:
+                continue
+            return l < r
+        elif isinstance(l, list) and isinstance(r, list):
+            res = cmp_nested_list(l, r)
+            if res == None:
+                continue
+            return res
+        elif isinstance(l, int) and isinstance(r, list):
+            return cmp_nested_list([l], r)
+        elif isinstance(l, list) and isinstance(r, int):
+            return cmp_nested_list(l, [r])
+
+    # All items compare equal, are lists same length?
+    if len(left) == len(right):
+        return None
+
+    return len(left) < len(right)
+
+
+
+def day13(filename):
+    print()
+    print(filename)
+
+    packet_pairs = []
+    with open(filename) as puzzlein:
+        packets = puzzlein.read()
+
+        for pair in packets.split("\n\n"):
+            packet_pairs.append(map(eval, pair.strip().split("\n")))
+
+
+    right_order_sum = 0
+    for ix, (left, right) in enumerate(packet_pairs):
+        if cmp_nested_list(left, right):
+            right_order_sum += 1 + ix
+        print(ix, left, right)
+
+    print("part1:", right_order_sum)
+
+
+
+
+logging.getLogger().setLevel(logging.DEBUG)
+
+day13(examples("13"))
+day13(inputs("13"))
