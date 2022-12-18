d4d9a754709496cee0ce82b858b847a23aee11d9
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-18 11:38:50 +0200

    clean day18.py

--- aoc22/py/day18.py
+++ aoc22/py/day18.py
@@ -1,133 +1,32 @@
 #!/usr/bin/env python
 
-from utils import inputs, examples
-
-import functools
-import json
-import logging
-import math
-
-import more_itertools
-
-from collections import defaultdict
-from dataclasses import dataclass
-
-import time
-import scipy
-
-
-
-import networkx as nx
 import numpy as np
-import itertools
+from scipy.ndimage import binary_fill_holes, correlate, generate_binary_structure
+
+from utils import examples, inputs
+
+
+manhattan_neighbours = generate_binary_structure(3, 1)
+manhattan_neighbours[1, 1, 1] = False
+
 
 def day18(filename):
     print()
     print(filename)
 
-    dimensions = [(0, 0), (0, 0), (0, 0)]
-    with open(filename) as puzzlein:
-        data = []
-        for line in puzzlein:
-            droplet = tuple(map(int, line.split(",")))
-            data.append(droplet)
-            for coordinate in range(3):
-                dimensions[coordinate] = min(dimensions[coordinate][0], droplet[coordinate]), max(dimensions[coordinate][1], droplet[coordinate])
+    data = np.loadtxt(filename, delimiter=",", dtype=int).transpose()
+    grid = np.zeros(shape=1 + data.max(axis=1), dtype=int)
+    grid[data[0], data[1], data[2]] = True
 
+    neighbour_droplets = correlate(grid, manhattan_neighbours, mode="constant", cval=0)
+    filled_up = binary_fill_holes(grid, structure=manhattan_neighbours)
 
-    # Check bottom 0
-    grid = np.zeros(shape=(dimensions[0][1] + 1, dimensions[1][1] + 1, dimensions[2][1] + 1), dtype=int)
+    all_surfaces = np.where(grid, 6 - neighbour_droplets, 0).sum()
+    interior_surfaces = np.where(filled_up - grid, neighbour_droplets, 0).sum()
 
-    for droplet in data:
-        grid[droplet[0], droplet[1], droplet[2]] = 1
+    print("part1:", all_surfaces)
+    print("part2:", all_surfaces - interior_surfaces)
 
-
-    #for point in np.ndindex(grid.shape):
-        #    if grid[point]:
-            #surroundings = grid[point[0] - 1:point[0] + 1, point[1] - 1:point[1] + 1, point[2] - 1, point[2] + 1]
-            #print(point)
-    
-    #numpy.array(
-    #connections = nx.Graph()
-    #for droplet in data:
-    #    connections.add_node(droplet)
-
-
-    neighbours = np.array(
-        [[[False, False, False],
-          [False,  True, False],
-          [False, False, False]],
-
-         [[False,  True, False],
-          [ True, False,  True],
-          [False,  True, False]],
-
-         [[False, False, False],
-          [False,  True, False],
-          [False, False, False]]])
-
-
-    neighdexes = np.array([
-        ( 0,  0, -1),
-        ( 0, -1,  0),
-        (-1 , 0,  0),
-        ( 0,  0,  1),
-        ( 0,  1,  0),
-        ( 1 , 0,  0),
-    ])
-
-
-
-    connected = scipy.ndimage.correlate(grid, neighbours, mode='constant', cval=0)
-
-
-    sides = 0
-    for point in np.ndindex(grid.shape):
-        if grid[point]:
-            #breakpoint()
-            this_sides = 6 - connected[point]
-            sides += this_sides
-            print(point, this_sides, sides)
-
-
-    print("part1:", sides)
-
-    exterior = 0
-
-    filled_up = scipy.ndimage.binary_fill_holes(grid, structure=neighbours)
-
-    #exterior = sides - 6 * (filled_up - grid).sum()
-    exterior = sides
-    #filled_connected = scipy.ndimage.correlate(filled_up, neighbours, mode='constant', cval=0)
-    for point in np.ndindex(grid.shape):
-        if (filled_up - grid)[point]:
-            #print(point)
-            #:w
-            #breakpoint()
-
-            for direc in neighdexes:
-                if grid[point[0] + direc[0], point[1] + direc[1], point[2] + direc[2]]:
-                    exterior -= 1
-            #for neighp in neighdexes + point:
-                #    print(grid[*neighp])
-            
-            #    #breakpoint()
-            #this_sides = 6 - filled_connected[point]
-            #exterior += this_sides
-            #print(point, this_sides, exterior)
-
-    print("part2:", exterior)
-
-    #print(data)
-
-    breakpoint()
-
-
-
-
-
-
-logging.getLogger().setLevel(logging.WARN)
 
 day18(examples("18"))
 day18(inputs("18"))
