a6a66f5966c1eaca7c1f17483defb18a3e12260b
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-19 11:11:54 +0200

    wip

--- aoc22/py/day19.py
+++ aoc22/py/day19.py
@@ -36,111 +36,99 @@ def day19(filename):
     # I guess we just have to try all choices
 
     qualities = []
+    mgeodes = []
     for recipe in data:
         bid, ore, clay, obsidian_ore, obsidian_clay, geode_ore, geode_obsidian = recipe
-        states = { ((0, 0, 0, 0), (1, 0, 0, 0)) }
+        states = { (1, 0, 0, 0): [[0, 0, 0, 0]] }
         max_geode = -1
         for minute in range(1, 24 + 1):
             minutes_left = 24 - minute
-            print("Minute", minute, "for", bid)
+            print("Minute", minute, "for", bid, "starting with", sum(len(v) for v in states.values()), "states")
 
-            next_states = set()
+            next_states = defaultdict(list)
 
-            for (resources, robots) in states:
+            for (robots, resources_list) in states.items():
+                for resources in resources_list:
 
-                possible_choices = [0, 0, 0, 0]
-                # TODO: is it faster to construct more clay robots or more obsidian robots?
-                # TODO: munge input data to have uniform comparison in resource cost
-                if resources[0] >= geode_ore and resources[2] >= geode_obsidian:
-                    possible_choices[3] = 1
-                if resources[0] >= obsidian_ore and resources[1] >= obsidian_clay:
-                    possible_choices[2] = 1
-                if resources[0] >= clay:
-                    possible_choices[1] = 1
-                if resources[0] >= ore:
-                    possible_choices[0] = 1
+                    possible_choices = [0, 0, 0, 0]
+                    # TODO: is it faster to construct more clay robots or more obsidian robots?
+                    # TODO: munge input data to have uniform comparison in resource cost
+                    if resources[0] >= geode_ore and resources[2] >= geode_obsidian:
+                        possible_choices[3] = 1
+                    if resources[0] >= obsidian_ore and resources[1] >= obsidian_clay:
+                        possible_choices[2] = 1
+                    if resources[0] >= clay:
+                        possible_choices[1] = 1
+                    if resources[0] >= ore:
+                        possible_choices[0] = 1
 
 
-                for ix, v in enumerate(possible_choices):
-                    if v:
-                        next_resources = list(resources)
+                    for ix, v in enumerate(possible_choices):
+                        if v:
+                            next_resources = resources.copy()
 
-                        for jx, V in enumerate(robots):
-                            next_resources[jx] += V
+                            for jx, V in enumerate(robots):
+                                next_resources[jx] += V
 
-                        next_robots = list(robots)
-                        next_robots[ix] += 1
+                            next_robots = list(robots)
+                            next_robots[ix] += 1
 
-                        if ix == 3:
-                            next_resources[0] -= geode_ore
-                            next_resources[2] -= geode_obsidian
-                        elif ix == 2:
-                            next_resources[0] -= obsidian_ore
-                            next_resources[1] -= obsidian_clay
-                        elif ix == 1:
-                            next_resources[0] -= clay
-                        elif ix == 0:
-                            next_resources[0] -= ore
-                        
-                        if next_resources[-1] > max_geode:
-                            max_geode = next_resources[-1]
+                            if ix == 3:
+                                next_resources[0] -= geode_ore
+                                next_resources[2] -= geode_obsidian
+                            elif ix == 2:
+                                next_resources[0] -= obsidian_ore
+                                next_resources[1] -= obsidian_clay
+                            elif ix == 1:
+                                next_resources[0] -= clay
+                            elif ix == 0:
+                                next_resources[0] -= ore
+                            
+                            if next_resources[-1] > max_geode:
+                                max_geode = next_resources[-1]
 
-                        if max_geode - next_resources[-1] > minutes_left * rx[-1] + (minutes_left + 1)*minutes_left / 2:
-                            #print("Skipping", next_resources, "since", max_geode, "unbeatable at", minute)
-                            continue
+                            if max_geode - next_resources[-1] > minutes_left * next_resources[-1] + (minutes_left + 1)*minutes_left / 2:
+                                #print("Skipping", next_resources, "since", max_geode, "unbeatable at", minute)
+                                continue
 
-                        next_states.add((tuple(next_resources), tuple(next_robots)))
-                    
+                                
+                            cands = next_states[tuple(next_robots)] 
+                            for other_resources in cands:
+                                # If there is the same amount of robots but with less resources,
+                                # don't keep that state
+                                if (next_resources[0] <= other_resources[0] and 
+                                   next_resources[1] <= other_resources[1] and 
+                                   next_resources[2] <= other_resources[2] and 
+                                   next_resources[3] <= other_resources[3]):
+                                    break
+                            else:
+                                next_states[tuple(next_robots)].append(next_resources)
+                                
+                    # Or, do nothing
+                    next_resources = resources.copy()
 
-                # Or, do nothing
-                next_resources = list(resources)
+                    if next_resources[-1] > max_geode:
+                        max_geode = next_resources[-1]
 
-                if next_resources[-1] > max_geode:
-                    max_geode = next_resources[-1]
-
-                for jx, V in enumerate(robots):
-                    next_resources[jx] += V
-                next_states.add((tuple(next_resources), robots))
-                    
+                    for jx, V in enumerate(robots):
+                        next_resources[jx] += V
+                    next_states[robots].append(next_resources)
 
             
-            max_states = defaultdict(list)
-            for (rx, ro) in next_states:
-                max_states[ro].append(rx)
+            states = next_states
 
-
-            # If there is the same amount of robots but with less resources,
-            # don't keep that state
-            states = set()
-
-            for ro, res in max_states.items():
-                for rx in res:
-                    alts = sum([rx[0] <= ro[0] and rx[1] <= ro[1] and rx[2] <= ro[2] and rx[3] <= ro[3] for ro in res])
-                    if alts > 1:
-                        # There is another set of resources with the same robot
-                        # that has more of at least one resource and no less of the rest
-                        # so we can ignore this one
-                        continue
-
-                    states.add((rx, ro))
-                        #print("Found an alternative")
                 
-                
-        max_geodes = -1
-        for s in states:
-            if s[0][-1] > max_geodes:
-                max_geodes =  s[0][-1]
-
-        ql = bid * max_geodes
+        ql = bid * max_geode
         qualities.append(ql)
-        print("quality level for", bid, ql, max_geodes)
+        mgeodes.append(max_geode)
+        print("quality level for", bid, ql, max_geode)
 
     
     print(data)
     print("part1:", sum(qualities))
+    print("part2:", mgeodes[0] * mgeodes[1] * mgeodes[2])
     breakpoint()
-    print("part2:", part2)
 
 
 day19(examples("19"))
-#day19(inputs("19"))
+day19(inputs("19"))
