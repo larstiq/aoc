fab8511ed529d62bffec25dbc7e3eff3ba1cf4d0
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-20 19:52:08 +0200

    meh

--- aoc22/py/day20.py
+++ aoc22/py/day20.py
@@ -2,7 +2,7 @@
 
 from utils import examples, inputs
 
-from collections import defaultdict
+from collections import defaultdict, deque
 import time
 
 import numpy as np
@@ -14,7 +14,6 @@ from dataclasses import dataclass
 @dataclass
 class Positioned:
     value: int
-    orig_pos: int
     touched: bool
 
 def day20(filename):
@@ -25,11 +24,12 @@ def day20(filename):
     part2 = 0
     data = np.loadtxt(filename, dtype=int)
 
+    #data = [1, 3, 4, -5]
     print(data)
 
 
 
-    positions = [Positioned(v, ix, False) for ix, v in enumerate(data)]
+    positions = [Positioned(v, False) for v in data]
 
 
     #breakpoint()
@@ -44,74 +44,92 @@ def day20(filename):
 
         assert positions[jx] == pos
 
-        if pos.value == 3:
-            assert [1, 2, 3, -2, -3, 0, 4] == [pos.value for pos in positions]
-            print("Good up to 3")
+        if "example" in str(filename):
 
-        if pos.value == -2:
-            assert [1, 2, -2, -3, 0, 3, 4] == [pos.value for pos in positions]
-            print("Good up to -2")
+            if pos.value == 3:
+                assert [1, 2, 3, -2, -3, 0, 4] == [pos.value for pos in positions]
+                print("Good up to 3")
+
+            if pos.value == -2:
+                assert [1, 2, -2, -3, 0, 3, 4] == [pos.value for pos in positions]
+                breakpoint()
+                print("Good up to -2")
+
+            if pos.value == 0:
+                assert [1, 2, -3, 0, 3, 4, -2] == [pos.value for pos in positions]
+                print("Good up to 0")
+
+            if pos.value == 4:
+                breakpoint()
+
+        # For 4 we need len(data), why -1 in the other case? Is it on the negativity?
 
 
-        if pos.value == 0:
-            pos.touched = True
-            continue
-
-        new_index = (jx + pos.value) % len(data)
-
-        # Careful, changes the positioning
-        print("    Positioning", pos, "currently at", jx)
         removed = False
 
-        if abs(pos.value) > len(data):
-            breakpoint()
-            positions.remove(pos)
+        if pos.value > 0:
+            new_index = (jx + pos.value) % len(data)
+        elif pos.value < 0:
+            new_index = (jx + pos.value) % (len(data) - 1)
+        elif pos.value == 0:
+            pos.touched = True
+            removed = True
+            print("    Positioning", pos, "currently at", jx, "then", new_index)
+            #print([pos.value for pos in positions])
+            continue
+
+
+        #  If we're moving the item to the left or right by less than len(data) we're not skippg over ourselves
+        #
+        #       0  1  2  3  4
+        #  -5: [1, |, 2, 4, 5]  2 + -5
+        #  -4: [1, 2, |, 4, 5]
+        #  -3: [1, 2, 4, |, 5]
+        #  -2: [|, 1, 2, 4, 5]
+        #  -1: [1, |, 2, 4, 5]
+        #   0: [1, 2, |, 4, 5]
+        #   1: [1, 2, 4, |, 5]
+        #  +2: [1, 2, 4, 5, |]
+        #  +3: [1, |, 2, 4, 5]
+        #  +4: [1, 2, |, 4, 5]
+        #  +5: [1, 2, 4, |, 5]
+        # 
+
+        #
+        # [1, 2, -3, 0, 3, 4, -2]
+        #                  5      6 7 8 9
+        #  6  7   8  9
+
+        # TODO: would this be easier if we had space between the items? ix |-> 2*x
+
+        # Careful, changes the positioning
+        #print("    Positioning", pos, "currently at", jx, "then", new_index)
 
         if 0 < new_index < len(data) - 1:
-            if pos.value > 0:
-                positions[new_index:new_index+1] = [
-                    positions[new_index],
-                    Positioned(pos.value, pos.orig_pos, True),
-                ]
-            else:
-                positions[new_index:new_index+1] = [
-                    Positioned(pos.value, pos.orig_pos, True),
-                    positions[new_index],
-                ]
+            positions[new_index:new_index+1] = [
+                positions[new_index],
+                Positioned(pos.value,  True),
+            ]
+        elif new_index == 0:
+            positions.append(Positioned(pos.value, True))
         else:
-            if new_index == 0:
-                positions.append(Positioned(pos.value, pos.orig_pos, True))
-                positions.remove(pos)
-                continue
-            elif new_index == len(data) - 1:
-                positions = [Positioned(pos.value, pos.orig_pos, True)] + positions
-                positions.remove(pos)
-                continue
-
             breakpoint()
-            if pos.value > 0:
-                positions[new_index:new_index+1] = [
-                    positions[new_index],
-                    Positioned(pos.value, pos.orig_pos, True),
-                ]
-            else:
-                positions[new_index:new_index+1] = [
-                    Positioned(pos.value, pos.orig_pos, True),
-                    positions[new_index],
-                ]
-
+            assert new_index == len(data) - 1
+            positions.append(Positioned(pos.value, True))
 
         if not removed:
             positions.remove(pos)
 
         
         #print([(pos.value, pos.touched) for pos in positions])
-        print([pos.value for pos in positions])
+        #print([pos.value for pos in positions])
 
 
     (zix, zero_pos), = [(ix, p) for (ix, p) in enumerate(positions) if p.value == 0]
 
     breakpoint()
+    print([positions[ix % len(data)].value for ix in (zix + 1000, zix + 2000, zix + 3000)])
+
     enc = sum(positions[ix % len(data)].value for ix in (zix + 1000, zix + 2000, zix + 3000))
 
     print("part1:", enc)
@@ -120,4 +138,4 @@ def day20(filename):
 
 
 day20(examples("20"))
-#day20(inputs("20"))
+day20(inputs("20"))
