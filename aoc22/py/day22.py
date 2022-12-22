729b6c8019877a832676a7b5823e3c215192f1e8
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-22 18:07:20 +0200

    pfffft

--- aoc22/py/day22.py
+++ aoc22/py/day22.py
@@ -207,85 +207,101 @@ def day22(filename):
         return pos, ang
 
     def wrap_input(position, direction):
+        #     0  1x  3x 
+        #     +1   2x  4x
+        # 0
+        # +1     D-C-B
+        #        |2|1|
+        # 1x     E-X-A
+        #        |3|  
+        # 2x   E-Y-A    
+        #      |5|4|
+        # 3x   D-Z-B
+        #      |6|
+        #      C-B
+
+        # TODO: use direction to know which part of the face data to use
+        #       0: right, 1: down, etc
+
+        size = 50
+        row, col = position[0], position[1]
         pos, ang = None, None
-        if position[0] == 0:
-            # We stepped up out of 1 or 2
-            if 51 <= position[1] <= 100:
-                # It was 2, we need to go to 6 through DC
-                # warp: 2^6>
-                pos = [150 + (position[1] - 50), 1]
+        if row == 0:
+            # 1< or 2<
+            if size+1 <= col <= 2*size:
+                # warp: 2^DC>6
+                pos = [3*size + (col - size), 1]
                 ang = 0
-            elif 101 <= 150:
-                # It was 1, we need to got 6 through CB
-                # warp: 1^6^
-                pos = [200, position[1] - 100]
+            elif 2*size + 1 <= col <= 3*size:
+                # warp: 1^CB^6
+                pos = [4*size, col - 2*size]
                 ang = 3
-        elif position[0] == 51:
-            # Stepping down out of 1 into 3
-            # warp: 1v3<
+        elif row == size + 1:
+            # warp: 1vXA<3
             ang = 2
-            pos = [position[1], 100]
-        elif position[0] == 100:
-            # Out of 5 stepping up into 3
-            # warp: 5^3>
+            pos = [col - size, 2*size]
+        elif row == 2 * size:
+            # warp: 5^EY>3
             ang = 0
-            pos = [50 + position[1], 51]
-        elif position[0] == 151:
+            pos = [size + col, size + 1]
+        elif row == 3*size + 1:
             # down out of 4 into 6
-            # warp: 4v6<
+            # warp: 4vZB<6
             ang = 2
-            pos = [150 + position[1] - 50, 50]
-        elif position[0] == 201:
-            # down out of 6 into 1
-            # warp: 6v1v
+            pos = [3*size + col - size, 50]
+        elif row == 4*size + 1:
+            # warp: 6vCBv1
             ang = 1
-            pos = [1, 100 + position[1]]
-        elif position[1] == 0:
+            pos = [1, 2*size + col]
+
+        # Left-right
+        elif col == 0:
             # 5 or 6
-            if 101 <= position[0] <= 150:
-                # 5<-2
-                # warp: 5<2>
+            if 2*size+1 <= row <= 3*size:
+                # warp: 5<ED>2
                 ang = 0
                 # 101 -> 50
                 # 150 -> 1
-                pos = [151 - position[0], 51]
-            elif 151 <= position[0] <= 200:
-                # 6<-2
-                # warp: 6<2v
+                pos = [3*size + 1 - row, size+1]
+            elif 3*size+1 <= row <= 4*size:
+                # warp: 6<DCv2
                 ang = 1
-                pos = [1, 50 + position[0] - 150]
-        elif position[1] == 50:
+                pos = [1, size + row - 3*size]
+        elif col == size:
             # 2 or 3
-            if 1 <= position[0] <= 50:
-                # 2<-5
-                # warp
+            if 1 <= row <= size:
+                # warp: 2<DE>5
                 ang = 0
                 # 1 -> 150
                 # 50 -> 101
-                pos = [151 - position[0], 1]
-            elif 51 <= position[0] <= 100:
+                pos = [3*size + 1 - row, 1]
+            elif size+1 <= row <= 2*size:
                 # 3<-5
+                # warp: 3<EYv5
                 ang = 1
-                pos = [101, position[0] - 50] 
-        elif position[1] == 51:
-            # 6->4
+                pos = [2*size + 1, row - size] 
+        elif col == size+1:
+            # warp: 6>ZB^4
             ang = 3
-            pos = [150, 50 + position[0] - 150]
-        elif position[1] == 101:
+            pos = [3*size, size + row - 3*size]
+        elif col == 2*size + 1:
             # 3 or 4
-            if 51 <= position[0] <= 100:
+            if size+1 <= row <= 2*size:
                 # 3->1
+                # warp: 3>XA^1
                 ang = 3
-                pos = [50, 100 + position[0] - 50]
-            elif 101 <= position[0] <= 150:
+                pos = [size, 2*size + row - size]
+            elif 2*size+1 <= row <= 3*size:
                 ang = 2
+                # warp: 4>AB<1
                 # 101 -> 50
                 # 150 -> 1
-                pos = [151 - position[0], 150]
-        elif position[1] == 151:
+                pos = [3*size+1 - row, 3*size]
+        elif col == 3*size + 1:
             # 1->4
+            # warp: 1>BA<4
             ang = 2
-            pos = [151 - position[0], 50]
+            pos = [3*size + 1 - row, size]
 
 
         if pos is None or ang is None:
@@ -336,8 +352,8 @@ def day22(filename):
                     print("Stepped into", position, direction)
                     pathchar = {0: '>', 1: 'v', 2: '<', 3: '^'}[direction]
                     pathboard[position[0], position[1]] = pathchar
-                    print("\n".join("".join(line) for line in pathboard))
-                    breakpoint()
+                    #print("\n".join("".join(line) for line in pathboard))
+                    #breakpoint()
                 elif next_square == 2:
                     # Don't change position or angle if we'd step into a wall
                     stepped = True
@@ -363,5 +379,5 @@ def day22(filename):
     print("part2:", part2)
 
 
-day22(examples("22"))
-#day22(inputs("22"))
+#day22(examples("22"))
+day22(inputs("22"))
