f5b6540dc645580b5c97df0341555a324b95d85c
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2022-12-25 10:21:35 +0200

    bleh

--- aoc22/py/day25.py
+++ aoc22/py/day25.py
@@ -23,6 +23,7 @@ snafu2decimal = {
     "-": -1,
     "=": -2,
 }
+decimal2snafu = {v: k for (k, v) in snafu2decimal.items()}
 
 
 def snafu_to_list(snafu):
@@ -63,16 +64,45 @@ def peal(number):
 
 
 def decimal_to_list(number):
+    ix = largest_five_place(number)
     remainder = number
     five_places = {}
-    while remainder > 0:
-        next_coefficients, next_remainder = peal(remainder)
-        five_places.update(next_coefficients)
-        if number - next_remainder != sum(v*5**k for (k, v) in five_places.items()):
+    
+    while ix > -1:
+        if ix in five_places:
             breakpoint()
-        remainder = next_remainder
+            ix -= 1
+            continue
+        if 5**ix >= remainder:
+            if 5**ix <= remainder:
+                five_places[ix] = 1
+                remainder -= 5**ix
+            elif 5**ix - 5**(ix - 1) <= remainder:
+                five_places[ix] = 1
+                five_places[ix - 1] = -1
+                remainder -= 5**ix - 5**(ix - 1)
+            # This doens't work because in case of 1== we still need to pick 1= here.
+            elif 5**ix - 2*5**(ix - 1) <= remainder:
+                five_places[ix] = 1
+                five_places[ix - 1] = -2
+                remainder -= 5**ix - 2*5**(ix - 1)
+        elif 2*5**ix <= remainder:
+            breakpoint()
+            five_places[ix] = 2
+            remainder -= 2*5**ix
+        else:
+            five_places[ix] = 0
+
+        if number - remainder != sum(v*5**k for (k, v) in five_places.items()):
+            breakpoint()
+
+        ix -= 1
 
     return [v for (k, v) in sorted(five_places.items())]
+
+
+def list_to_snafu(lijst):
+    return "".join(decimal2snafu[d] for d in reversed(lijst))
 
 
 def day24(filename):
@@ -90,11 +120,14 @@ def day24(filename):
     print(data)
 
     as_decimals = [list_to_decimal(l) for l in data]
-    print(as_decimals)
-    print([decimal_to_list(d) for d in as_decimals])
+    print("as decimals", as_decimals)
+    print("decimals to lists", [decimal_to_list(d) for d in as_decimals])
+    print("snafus", [list_to_snafu(decimal_to_list(d)) for d in as_decimals])
 
     total_decimal = sum(as_decimals)
     print(total_decimal)
+    print(decimal_to_list(total_decimal))
+    part1 = list_to_snafu(decimal_to_list(total_decimal))
 
     print("part1:", part1)
     print("part2:", part2)
