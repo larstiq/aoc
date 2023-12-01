491a8092ea9796cb915d3c36749bbb83672d1d77
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-01 09:15:45 +0200

    WIP

--- aoc23/py/day01.py
+++ aoc23/py/day01.py
@@ -17,7 +17,7 @@ words = [
 "nine",
 ]
 
-regex = "0|1|2|3|4|5|6|7|8|9|zero|one|two|three|four|five|six|seven|eight|nine"
+regex = "0|1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine"
 regex_simple = "0|1|2|3|4|5|6|7|8|9"
 
 values = {
@@ -31,7 +31,6 @@ values = {
     "7": 7,
     "8": 8,
     "9": 9,
-    "zero": 0,
     "one": 1,
     "two": 2,
     "three": 3,
@@ -43,6 +42,18 @@ values = {
     "nine": 9,
 }
 
+word_to_digit = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
 def day01(filename):
     print()
     print(filename)
@@ -50,28 +61,33 @@ def day01(filename):
     with open(filename) as puzzlein:
         total = 0
         word_total = 0
+        calibs = []
         for line in puzzlein:
-
-            digits = [(ix, char) for (ix, char) in enumerate(line) if char in "0123456789"]
-
 
             results = re.findall(regex, line)
             simple = re.findall(regex_simple, line)
-            #print(results)
-            #print(results[0], results[-1])
-            #print(values[results[0]], values[results[-1]])
+
+            #if len(results) < 2:
+            #    breakpoint()
 
             number = int(str(values[results[0]]) + str(values[results[-1]]))
-            print(line, number)
+            #print(line, number)
+
+            if str(number) != word_to_digit.get(results[0], results[0]) + word_to_digit.get(results[-1], results[-1]):
+                breakpoint()
 
             #simple_number = int(simple[0] + simple[-1])
-
             #total += simple_number
-            breakpoint()
             word_total += number 
+            calibs.append(number)
+            print(line, number, results)
+            breakpoint()
 
         print(total)
         print(word_total)
+        print(calibs)
+        print(sum(calibs))
+        print(len(calibs))
 
 
     print("part1", total)
