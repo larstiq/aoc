b858a38d84515e37ccdeaad6aeebc052b71395d8
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-01 07:47:05 +0200

    WIP

--- aoc23/py/day01.py
+++ aoc23/py/day01.py
@@ -2,26 +2,80 @@
 
 from utils import inputs, examples
 
+import re
+
+words = [
+"zero",
+"one",
+"two",
+"three",
+"four",
+"five",
+"six",
+"seven",
+"eight",
+"nine",
+]
+
+regex = "0|1|2|3|4|5|6|7|8|9|zero|one|two|three|four|five|six|seven|eight|nine"
+regex_simple = "0|1|2|3|4|5|6|7|8|9"
+
+values = {
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
 def day01(filename):
     print()
     print(filename)
 
     with open(filename) as puzzlein:
-        elves = []
-        elf = 0
+        total = 0
+        word_total = 0
         for line in puzzlein:
-            if line == "\n":
-                elves.append(elf)
-                elf = 0
-            else:
-                elf += int(line.strip())
 
-        if elf > 0:
-            elves.append(elf)
+            digits = [(ix, char) for (ix, char) in enumerate(line) if char in "0123456789"]
 
-    print("part1", max(elves))
-    print("part2", sum(sorted(elves)[-3:]))
+
+            results = re.findall(regex, line)
+            simple = re.findall(regex_simple, line)
+            #print(results)
+            #print(results[0], results[-1])
+            #print(values[results[0]], values[results[-1]])
+
+            number = int(str(values[results[0]]) + str(values[results[-1]]))
+            print(line, number)
+
+            #simple_number = int(simple[0] + simple[-1])
+
+            #total += simple_number
+            breakpoint()
+            word_total += number 
+
+        print(total)
+        print(word_total)
+
+
+    print("part1", total)
+    print("part2", word_total)
 
 
 day01(examples("01"))
