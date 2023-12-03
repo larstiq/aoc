f26df3b08a38327d405fd60a27681173ce3a728d
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-03 08:15:32 +0200

    Half right

--- aoc23/py/day03.py
+++ aoc23/py/day03.py
@@ -13,6 +13,21 @@ import itertools
 
 import string
 
+
+
+def display_blizzards(blizzards):
+    symbols = '><v^'
+    out = []
+    for ix in range(blizzards.shape[0]):
+        out.append("\n")
+        for jx in range(blizzards.shape[1]):
+           if isinstance(blizzards[jx][ix], float):
+              c = " "
+           else:
+              c = blizzards[jx][ix]
+           out.append(c)
+    print("".join(out))
+
 def day03(filename):
     print()
     print(filename)
@@ -34,32 +49,44 @@ def day03(filename):
 
     symbol_pos = df.isin(real_symbols)
 
-    for direction in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        for pos in df[df.isin(real_symbols)].stack().index:
-            try:
-                ding = df[pos[0] + direction[0]][pos[1] + direction[1]] 
-            except:
-                break
+    import scipy
 
-            if ding in(string.digits):
-                for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    for i in itertools.count():
-                        try:
-                            dong = df[pos[0] + direction[0] + i*dx][pos[1] + direction[1] + i*dy]
-                        except:
-                            pass
-                            in (string.digits):
-                    while 
 
-                    print(pos[0] + direction[0] + i*dx,pos[1] + direction[1] + i*dy)
-                    print(pos[0] + direction[0] + 0*dx,pos[1] + direction[1] + 0*dy)
+    maybe_partts = df.isin(set(string.digits))
 
+    structure = scipy.ndimage.generate_binary_structure(2, 2)
+
+    neighbours = scipy.ndimage.binary_dilation(symbol_pos, structure) & maybe_partts
+    prev = maybe_partts
+    nextn = scipy.ndimage.binary_dilation(symbol_pos, structure) & maybe_partts
+    leftright = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
+    while not prev.equals(nextn):
+        prev = nextn
+        nextn = scipy.ndimage.binary_dilation(nextn, leftright) & maybe_partts
+
+
+    breakpoint()
+    parts = []
+    for ix, row in df[nextn].iterrows():
+        accum = []
+        for char in row:
+            if isinstance(char, float):
+                if accum != []:
+                    parts.append(int(''.join(accum)))
+                    accum = []
+            elif char in string.digits:
+                accum.append(char)
+            else:
+                print("Error!")
+                breakpoint()
+
+    breakpoint()
 
     print(df)
-    breakpoint()
+    part1 = sum(parts)
     print("part1:", part1)
     print("part2:", part2)
 
 
 day03(examples("03"))
-#day03(inputs("03"))
+day03(inputs("03"))
