905cfad9fafab8b59db28167241b05b231b15351
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-03 15:34:59 +0200

    mkay

--- aoc23/py/day03.py
+++ aoc23/py/day03.py
@@ -14,7 +14,12 @@ import itertools
 import string
 import scipy
 
-from scipy.ndimage import generate_binary_structure, binary_dilation, binary_propagation, label
+from scipy.ndimage import (
+    generate_binary_structure,
+    binary_dilation,
+    binary_propagation,
+    label,
+)
 
 digits = set(string.digits)
 
@@ -38,32 +43,55 @@ def day03(filename):
 
     real_symbols = symbols - digits - set(".")
 
-    symbol_pos = df.isin(real_symbols)
-    maybe_partts = df.isin(digits)
-    structure = generate_binary_structure(2, 2)
+    maybe_parts = df.isin(digits)
+    friends = generate_binary_structure(2, 2)
     leftright = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
 
-    neighbours = binary_dilation(symbol_pos, structure) & maybe_partts
-    nextn = binary_propagation(neighbours, leftright, mask=maybe_partts) & maybe_partts
-    breakpoint()
+    digits_bordering_symbols = (
+        binary_dilation(df.isin(real_symbols), friends) & maybe_parts
+    )
+    parts_bordering_symbols = (
+        binary_propagation(digits_bordering_symbols, leftright, mask=maybe_parts)
+        & maybe_parts
+    )
+    # Use the visual separation to make a list of contiguous numbers
+    part_numbers = [
+        int(x)
+        for x in "".join(
+            "".join(row) for row in df[parts_bordering_symbols].fillna(" ").values
+        ).split(" ")
+        if x != ""
+    ]
+    part1 = sum(part_numbers)
 
-    lelijk = [int(x) for x in ''.join(''.join(row) for row in df[nextn].fillna(" ").values).split(" ") if x != '']
-    part1 = sum(lelijk)
-    
-    gears = df.isin(set('*'))
+
+    gears = df.isin(set("*"))
     gear_ratios = []
 
     for gear_pos in df[gears].stack().index:
-        block = df[gear_pos[0] -1:gear_pos[0] + 2][[gear_pos[1] - 1, gear_pos[1], gear_pos[1] + 1]]
+        block = df[gear_pos[0] - 1 : gear_pos[0] + 2][
+            [gear_pos[1] - 1, gear_pos[1], gear_pos[1] + 1]
+        ]
         labels, nb = label(block.isin(digits))
         if nb > 1:
-            real_gear_pos = (df == 0)
+            real_gear_pos = df == 0
             real_gear_pos[gear_pos[1]][gear_pos[0]] = True
-            neighbours = binary_dilation(real_gear_pos, structure) & maybe_partts
-            nextn = binary_propagation(neighbours, leftright, mask=maybe_partts) & maybe_partts
-            lelijk = [int(x) for x in ''.join(''.join(row) for row in df[nextn].fillna(" ").values).split(" ") if x != '']
+            # breakpoint()
+            digits_bordering_gears = (
+                binary_dilation(real_gear_pos, friends) & maybe_parts
+            )
+            parts_bordering_gears = (
+                binary_propagation(digits_bordering_gears, leftright, mask=maybe_parts)
+                & maybe_parts
+            )
+            lelijk = [
+                int(x)
+                for x in "".join(
+                    "".join(row) for row in df[parts_bordering_gears].fillna(" ").values
+                ).split(" ")
+                if x != ""
+            ]
             gear_ratios.append(math.prod(lelijk))
-
 
     part2 = sum(gear_ratios)
 
