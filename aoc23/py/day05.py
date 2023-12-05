de5ac05aa9714e679ec44cfae4346ce62d6150e1
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-05 16:02:03 +0200

    works

--- aoc23/py/day05.py
+++ aoc23/py/day05.py
@@ -16,6 +16,37 @@ from portion import closed as Interval, OPEN
 def shift(interval, offset):
     return Interval(interval.lower + offset, interval.upper + offset)
 
+
+def map_intervals(dest, source, lengte, current, next_state):
+    out_range = Interval(source, source + lengte - 1)
+
+    carry = []
+    while len(current) > 0:
+        seed_range = current.pop()
+        offset = dest - source
+        # Skip no intersections
+        if seed_range.upper < out_range.lower or seed_range.lower > out_range.upper:
+            # No intersection, put it to the side for now
+            carry.append(seed_range)
+            continue
+
+        intersection = seed_range.intersection(out_range)
+        next_state.append(shift(intersection, offset))
+
+        if intersection == seed_range:
+            continue
+
+        for remainder in seed_range - intersection:
+            if remainder.left == OPEN:
+                current.append(Interval(remainder.lower + 1, remainder.upper))
+            elif remainder.right == OPEN:
+                current.append(Interval(remainder.lower, remainder.upper - 1))
+            else:
+                breakpoint()
+
+    current += carry
+
+
 def day05(filename):
     print()
     print(filename)
@@ -23,19 +54,21 @@ def day05(filename):
     part1 = 0
     part2 = 0
 
-    seeds = []
-    seeds1 = []
-    seeds2 = []
     stage = "seed"
 
-    current = []
-    next_state = []
+    current = {}
+    current["part1"] = []
+    current["part2"] = []
+    next_state = {}
+    next_state["part1"] = []
+    next_state["part2"] = []
     with open(filename) as puzzlein:
         for line in puzzlein:
             if line.startswith("seeds:"):
                 pair = []
                 pre_seeds = [ int(x) for x in line.split(":")[1].split() ]
                 seeds1 = [Interval(x, x) for x in pre_seeds]
+                seeds2 = []
                 for entry in pre_seeds:
                     if len(pair) < 2:
                         pair.append(entry)
@@ -44,61 +77,33 @@ def day05(filename):
                         pair = [entry]
 
                 seeds2.append(Interval(pair[0], pair[0] + pair[1] -1))
-                next_state = seeds2
+                next_state["part1"] = seeds1
+                next_state["part2"] = seeds2
             elif line == "\n":
                 continue
             elif ":" in line and line.split(":")[0].split()[1] == "map":
                 map_name = line.split(":")[0].split()[0]
                 # If no mapping then input/output are the same number so continue with leftovers
-                current += next_state
-                next_state = []
+                current["part1"].extend(next_state["part1"])
+                current["part2"].extend(next_state["part2"])
+                next_state["part1"] = []
+                next_state["part2"] = []
             elif line.strip() == "":
                 continue
             else:
                 dest, source, lengte = [int(x) for x in line.split()]
+                map_intervals(dest, source, lengte, current["part1"], next_state["part1"])
+                map_intervals(dest, source, lengte, current["part2"], next_state["part2"])
 
-                assert lengte > 0
-                out_range = Interval(source, source + lengte - 1)
 
-                carry = []
-                while len(current) > 0:
-                    
-                    seed_range = current.pop()
-                    offset = dest - source
-                    # Skip no intersections
-                    if seed_range.upper < out_range.lower or seed_range.lower > out_range.upper:
-                        # No intersection, put it to the side for now
-                        carry.append(seed_range)
-                        continue
-
-                    intersection = seed_range.intersection(out_range)
-                    next_state.append(shift(intersection, offset))
-
-                    if intersection == seed_range:
-                        continue
-
-                    for remainder in seed_range - intersection:
-                        if remainder.left == OPEN:
-                            current.append(Interval(remainder.lower + 1, remainder.upper))
-                        elif remainder.right == OPEN:
-                            current.append(Interval(remainder.lower, remainder.upper - 1))
-                        else:
-                            breakpoint()
-
-                current += carry
-
-    print(current)
-    print(next_state)
-    starts = {interval.lower for interval in current}
-    startstwee = {interval.lower for interval in next_state}
-    part2 = min(startstwee)
+    part1 = min(interval.lower for interval in next_state["part1"])
+    part2 = min(interval.lower for interval in next_state["part2"])
 
     if str(filename).endswith("inputs/05"):
-        #assert part1 == 662197086
+        assert part1 == 662197086
         assert part2 == 52510809
-    # breakpoint()
 
-    #print("part1:", part1)
+    print("part1:", part1)
     print("part2:", part2)
 
 
