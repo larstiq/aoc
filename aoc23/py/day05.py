426ca83cb36f097178941370af0640d501e85296
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-05 13:52:45 +0200

    working

--- aoc23/py/day05.py
+++ aoc23/py/day05.py
@@ -10,7 +10,11 @@ import pandas as pd
 import networkx as nx
 
 import itertools
-Interval = pd.RangeIndex
+from portion import closed as Interval, OPEN
+
+
+def shift(interval, offset):
+    return Interval(interval.lower + offset, interval.upper + offset)
 
 def day05(filename):
     print()
@@ -20,6 +24,8 @@ def day05(filename):
     part2 = 0
 
     seeds = []
+    seeds1 = []
+    seeds2 = []
     stage = "seed"
 
     current = []
@@ -36,88 +42,84 @@ def day05(filename):
                         seeds.append(Interval(pair[0], pair[0] + pair[1] - 1))
                         pair = [entry]
 
-                print("pair", pair)
+                #print("pair", pair)
                 seeds.append(Interval(pair[0], pair[0] + pair[1] -1))
                 next_state = seeds
             elif line == "\n":
                 continue
             elif ":" in line and line.split(":")[0].split()[1] == "map":
                 map_name = line.split(":")[0].split()[0]
-                print("Pre", map_name, current, next_state)
-                # Unchanged stays
-                #breakpoint()
+                #print("Pre", map_name, current, next_state)
                 aap = next_state + current
-                assert sum(map(lambda x: len(x) + 1, seeds)) == sum(map(lambda x: len(x) + 1, aap))
+                #assert sum(map(lambda x: len(x) + 1, seeds)) == sum(map(lambda x: len(x) + 1, aap))
                 current = aap
                 next_state = []
-                print("Swapped", map_name, current, next_state)
+                #print("Swapped", map_name, current, next_state)
             elif line.strip() == "":
                 continue
             else:
                 dest, source, lengte = [int(x) for x in line.split()]
 
-                print(map_name, dest, source, lengte)
+                #print(map_name, dest, source, lengte)
                 assert lengte > 0
                 out_range = Interval(source, source + lengte - 1)
                 carry = []
                 while len(current) > 0:
                     
                     seed_range = current.pop()
+                    offset = dest - source
                     # Skip no intersections
-                    if seed_range.stop < out_range.start or seed_range.start > out_range.stop:
+                    if seed_range.upper < out_range.lower or seed_range.lower > out_range.upper:
                         carry.append(seed_range)
                         continue
-                    elif seed_range.stop == out_range.start:
-                        print("SINGLETON!")
-                        #breakpoint()
-                        next_state.append(shift + Interval(seed_range.stop, seed_range.stop))
-                        assert seed_range.start < seed_range.stop
-                        carry.append(Interval(seed_range.start, seed_range.stop - 1))
+                    elif seed_range.upper == out_range.lower:
+                        next_state.append(shift(Interval(seed_range.upper, seed_range.upper), offset))
+                        assert seed_range.lower < seed_range.upper
+                        carry.append(Interval(seed_range.lower, seed_range.upper - 1))
                         continue
-                    elif seed_range.start == out_range.stop:
-                        print("SINGLETON!")
-                        #breakpoint()
-                        next_state.append(shift + Interval(seed_range.start, seed_range.start))
-                        assert seed_range.start < seed_range.stop
-                        carry.append(Interval(seed_range.start + 1, seed_range.stop))
+                    elif seed_range.lower == out_range.upper:
+                        next_state.append(shift(Interval(seed_range.lower, seed_range.lower), offset))
+                        assert seed_range.lower < seed_range.upper
+                        carry.append(Interval(seed_range.lower + 1, seed_range.upper))
                         continue
 
                     intersection = seed_range.intersection(out_range)
-                    shift = dest - source
-                    next_state.append(shift + intersection)
+                    next_state.append(shift(intersection, offset))
 
-                    print(f"Found intersection {(intersection.start, intersection.stop)} of {(out_range.start, out_range.stop)} and {(seed_range.start, seed_range.stop)}")
+                    #print(f"Found intersection {(intersection.start, intersection.stop)} of {(out_range.start, out_range.stop)} and {(seed_range.start, seed_range.stop)}")
 
-                    if intersection.equals(seed_range):
+                    if intersection == seed_range:
                         continue
 
-                    left_size = right_size = 0
-                    if intersection.start > seed_range.start:
-                        left = Interval(seed_range.start, min(intersection.start - 1, seed_range.stop))
-                        current.append(left)
-                        left_size = len(left) + 1
-                        print(f"    left {(left.start, left.stop, len(left))} of {(out_range.start, out_range.stop)} and {(seed_range.start, seed_range.stop)}")
-                    if intersection.stop < seed_range.stop:
-                        right = Interval(max(seed_range.start, intersection.stop + 1), seed_range.stop)
-                        current.append(right)
-                        right_size = len(right) + 1
-                        print(f"    right {(right.start, right.stop, len(right))} of {(out_range.start, out_range.stop)} and {(seed_range.start, seed_range.stop)}")
-
-                    assert left_size + right_size + len(intersection) + 1 == len(seed_range) + 1
+                    for remainder in seed_range - intersection:
+                        if remainder.left == OPEN:
+                            current.append(Interval(remainder.lower + 1, remainder.upper))
+                        elif remainder.right == OPEN:
+                            current.append(Interval(remainder.lower, remainder.upper - 1))
+                        else:
+                            breakpoint()
 
                 current += carry
 
 
     print(current)
     print(next_state)
-    starts = {interval.start for interval in current}
-    startstwee = {interval.start for interval in next_state}
-    part2 = min(starts)
+    starts = {interval.lower for interval in current}
+    startstwee = {interval.lower for interval in next_state}
+    part2 = min(startstwee)
+
+    if str(filename).endswith("inputs/05"):
+        #assert part1 == 662197086
+        assert part2 == 52510809
     # breakpoint()
 
-    print("part1:", part1)
+    #print("part1:", part1)
     print("part2:", part2)
 
 
 day05(examples("05"))
+import time
+start = time.time()
 day05(inputs("05"))
+end = time.time()
+print(end - start)
