d889ba4f85ba03d8adb8deae5f58e014fe572dca
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-07 09:03:06 +0200

    some

--- aoc23/py/day07.py
+++ aoc23/py/day07.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
-import math
-from collections import Counter, defaultdict, deque
+from collections import Counter
 
 from utils import examples, inputs
-
-import numpy as np
-import pandas as pd
-import networkx as nx
 
 import itertools
 
@@ -18,47 +13,40 @@ def day07(filename):
     part1 = 0
     part2 = 0
 
-    mapping = dict(zip(reversed("AKQJT98765432"), itertools.count()))
-    back = {v: k for (k, v) in mapping.items()}
     hands = {}
     with open(filename) as puzzlein:
         for line in puzzlein:
             hand, bid = line.strip().split()
-            hand = tuple([mapping[c] for c in hand])
             hands[hand] = int(bid)
 
-
-    def rank(left, right):
-        l = Counter(left)
-        r = Counter(right)
-        if l < r:
-            return -1
-        elif l > r:
-            return 1
-        else:
-            return -1 * (mapping[left[0]] > mapping[right[0]])
-
-
+    mapping = dict(zip(reversed("AKQJT98765432"), itertools.count()))
     def key(c):
         twee = list(reversed(sorted(Counter(c).values())))[:2]
         if len(twee) < 2:
             twee.append(0)
-        twee.append(c)
+        twee += [mapping[k] for k in c]
         return twee
 
-    import functools
-    sorte = list(hands.keys())
-    sorte.sort(key=key)
-    print(sorte)
-    #:w
-    # sorte.sort(key=functools.cmp_to_key(rank))
+    mapping2 = dict(zip(reversed("AKQT98765432J"), itertools.count()))
+    def key2(c):
+        counted = Counter(c)
+        if 'J' in counted:
+           jcount = counted["J"] 
+           del counted["J"]
+           if jcount < 5:
+               most = counted.most_common()[0][0]
+               counted[most] += jcount
+           else:
+               counted["J"] = jcount
 
-    backed = ["".join(back[c] for c in h) for h in sorte]
-    print(backed)
-
-    winnings = [(ix + 1)*hands[hand] for ix, hand in enumerate(sorte)]
+        twee = list(reversed(sorted(counted.values())))[:2]
+        if len(twee) < 2:
+            twee.append(0)
+        twee += [mapping2[k] for k in c]
+        return twee
     
-    part1 = sum(winnings)
+    part1 = sum((ix + 1)*hands[hand] for ix, hand in enumerate(sorted(hands, key=key)))
+    part2 = sum((ix + 1)*hands[hand] for ix, hand in enumerate(sorted(hands, key=key2)))
     print("part1:", part1)
     print("part2:", part2)
     breakpoint()
