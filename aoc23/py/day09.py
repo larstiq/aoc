a8a3e2dc97dadc9d28f9832ecd28b80492f8bd62
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-09 09:22:00 +0200

    np.diff

---
+++ aoc23/py/day09.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from utils import examples, inputs
+
+import numpy as np
+
+
+def day09(filename):
+    print()
+    print(filename)
+
+    part1 = 0
+    part2 = 0
+
+    histories = np.loadtxt(filename, dtype=int)
+
+    def delve(history):
+        next_level = np.diff(history)
+        deeper = delve(next_level) if next_level.any() else 0
+        return history[-1] + deeper
+
+    part1 = sum(delve(line) for line in histories)
+    part2 = sum(delve(line[::-1]) for line in histories)
+
+    print("part1:", part1)
+    print("part2:", part2)
+
+
+day09(examples("09"))
+day09(inputs("09"))
