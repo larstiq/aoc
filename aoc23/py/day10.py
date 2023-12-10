b08847eaa2aed78eab3fbb448e75d7da335fd0fd
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-10 16:25:39 +0200

    nope broke it

--- aoc23/py/day10.py
+++ aoc23/py/day10.py
@@ -21,42 +21,63 @@ def day10(filename):
 
     data = []
     graph = nx.DiGraph()
+    chars = {}
+
     with open(filename) as puzzlein:
-        for row, line in enumerate(puzzlein):
+        for halfrow, line in enumerate(puzzlein):
+            row = 2 * halfrow
             data.append(list(line.strip()))
-            for column, char in enumerate(line.strip()):
-                graph.add_node((row, column))
+            for halfcolumn, char in enumerate(line.strip()):
+                column = 2 * halfcolumn
 
+                graph.add_node((row, column), orig=char)
+                chars[(row, column)] = char
+
+                if (row, column) == (35, 208):
+                    breakpoint()
+
+                # Add neighbouring pipes and pieces inbetween on a doublesize grid
                 if char  == "|":
-                    graph.add_edge((row, column), (row - 1, column))
-                    graph.add_edge((row, column), (row + 1, column))
+                    graph.add_edge((row,     column), (row - 1, column))
+                    graph.add_edge((row - 1, column), (row - 2, column))
+                    graph.add_edge((row,     column), (row + 1, column))
+                    graph.add_edge((row + 1, column), (row + 2, column))
                 elif char == "-":
-                    graph.add_edge((row, column), (row, column - 1))
-                    graph.add_edge((row, column), (row, column + 1))
+                    graph.add_edge((row, column    ), (row, column - 1))
+                    graph.add_edge((row, column - 1), (row, column - 2))
+                    graph.add_edge((row, column    ), (row, column + 1))
+                    graph.add_edge((row, column + 1), (row, column + 2))
                 elif char == "L":
-                    graph.add_edge((row, column), (row - 1, column))
-                    graph.add_edge((row, column), (row, column + 1))
+                    graph.add_edge((row    , column    ), (row - 1, column))
+                    graph.add_edge((row - 1, column    ), (row - 2, column))
+                    graph.add_edge((row    , column    ), (row, column + 1))
+                    graph.add_edge((row    , column + 1), (row, column + 2))
                 elif char == "J":
-                    graph.add_edge((row, column), (row - 1, column))
-                    graph.add_edge((row, column), (row, column - 1))
+                    graph.add_edge((row,     column    ), (row - 1, column))
+                    graph.add_edge((row - 1, column    ), (row - 2, column))
+                    graph.add_edge((row,     column    ), (row, column - 1))
+                    graph.add_edge((row,     column - 1), (row, column - 2))
                 elif char == "7":
-                    graph.add_edge((row, column), (row + 1, column))
-                    graph.add_edge((row, column), (row, column - 1))
+                    graph.add_edge((row,     column    ), (row + 1, column))
+                    graph.add_edge((row + 1, column    ), (row + 2, column))
+                    graph.add_edge((row,     column    ), (row, column - 1))
+                    graph.add_edge((row,     column - 1), (row, column - 2))
                 elif char == "F":
-                    graph.add_edge((row, column), (row + 1, column))
-                    graph.add_edge((row, column), (row, column + 1))
-
-    df = pd.DataFrame(data)
-
-    # Double the grid so what looks enclosed but is on the outside
-    # has a connection to the outside
-    ddf = pd.DataFrame(data=np.nan, columns=np.arange(0, df.columns[-1] + .5, .5), index=np.arange(0, df.index[-1] + .5, .5)).combine_first(df).fillna('M')
-    start = df[df == 'S'].stack().index[0]
+                    graph.add_edge((row,     column    ), (row + 1, column))
+                    graph.add_edge((row + 1, column    ), (row + 2, column))
+                    graph.add_edge((row,     column    ), (row, column + 1))
+                    graph.add_edge((row,     column + 1), (row, column + 2))
+                elif char == "S":
+                    start = (row, column)
 
     neighbours = graph.to_undirected()[start]
     assert len(neighbours) == 2
     for node in neighbours:
         graph.add_edge(start, node)
+        diff = node[0] - start[0], node[1] - start[1]
+        extended = (node[0] + diff[0], node[1] + diff[1])
+        graph.add_edge(node, extended)
+        print("Start neighbours", start, node, extended)
 
     distance = 1
     second = list(graph[start])[0]
@@ -66,7 +87,7 @@ def day10(filename):
     lefthands = set()
     righthands = set()
 
-    direction = second[0] - start[0], second[1] - second[0]
+    direction = second[0] - start[0], second[1] - start[1]
 
     def left(node, direction):
         # Left
@@ -93,11 +114,6 @@ def day10(filename):
             return (node[0], node[1] - 1)
 
 
-    animation = False
-    if animation:
-        traversal = np.zeros(df.shape, dtype=bool)
-        traversal[start[0]][start[1]] = True
-
     while True:
         maybe_left = left(current, direction)
         maybe_right = right(current, direction)
@@ -105,13 +121,6 @@ def day10(filename):
             lefthands.add(maybe_left)
         if maybe_right in graph:
             righthands.add(maybe_right)
-
-        if animation:
-            traversal[current[0]][current[1]] = True
-            display_dfield(
-                    df.where(traversal).dropna(how='all').dropna(how='all', axis='columns')
-            )
-            time.sleep(0.0001)
 
         nexts = set(graph[current]) - seen
         assert len(nexts) < 2
@@ -127,20 +136,17 @@ def day10(filename):
         current = nexts
         distance += 1
 
-    print("Connecting the pipes in the doubled grid")
-    for edge in graph.edges(seen):
-        mid = (edge[0][0] + edge[1][0])/2, (edge[0][1] + edge[1][1])/ 2
-        ddf[mid[1]][mid[0]] = 'P'
-    # And pipes on the original grid
+    part1 = len(seen) // 4
 
+    pijp = np.zeros((2 * len(data), 2 * len(data[0])), dtype=bool)
     for node in seen:
-        ddf[node[1]][node[0]] = 'P'
+        pijp[node[1]][node[0]] = True
 
-    print("... done connecting the pipes")
+    df = pd.DataFrame(data=data, index=range(0, pijp.shape[0], 2), columns=range(0, pijp.shape[1], 2))
+    ddf = pd.DataFrame(data=np.nan, index=range(0, pijp.shape[0]), columns=range(0, pijp.shape[1])).combine_first(df)
 
-    pijp = ddf == 'P'
-    part1 = math.ceil((len(seen) - 1) / 2)
-    inflated = ddf.where(scipy.ndimage.binary_fill_holes(pijp)).stack().value_counts()
+    #inflated = ddf.where(scipy.ndimage.binary_fill_holes(pijp)).stack().value_counts()
+    inflated = ddf.where(scipy.ndimage.binary_fill_holes(pijp)).stack()
     part2 = inflated.loc[~inflated.index.isin(('P', 'M'))].sum()
     print("part1:", part1)
     print("part2:", part2)
