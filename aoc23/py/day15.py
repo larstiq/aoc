1f5fda3eb44d7983aa4440d88e05bbb7b378609a
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-15 08:32:34 +0200

    15/2

--- aoc23/py/day15.py
+++ aoc23/py/day15.py
@@ -17,7 +17,8 @@ def HASH(s):
     for char in s:
         current += ord(char)
         current *= 17
-    return current % 256
+        current %= 256
+    return current
    # return functools.reduce(lambda x, y: (x + ord(y) * 17) % 256, list(s), 0)
 
 def day15(filename):
@@ -40,10 +41,41 @@ def day15(filename):
 
 
     hashes = []
+    labels2boxes = {}
+    boxes = defaultdict(list)
     for part in commands:
+        if "=" in part:
+            label, action, focus = part.partition("=")
+        else:
+            label, action = part[:-1], part[-1]
+
+        box = HASH(label)
+        if action == "-":
+            for (blabel, focus) in boxes[box][:]:
+                if blabel == label:
+                    boxes[box].remove((blabel, focus))
+
+        elif action == "=":
+            found = False
+            for ix, (blabel, bfocus) in enumerate(boxes[box][:]):
+                if blabel == label:
+                    boxes[box].insert(ix, (label, int(focus)))
+                    boxes[box].remove((blabel, bfocus))
+                    found = True
+                    break
+
+            if not found:
+                boxes[box].append((label, int(focus)))
+
         hashes.append(HASH(part))
 
     part1 = sum(hashes)
+
+    part2 = 0
+    for (boxid, lenses) in boxes.items():
+        ding = [ (1 + boxid) * (ix + 1) * focus for (ix, (label, focus)) in enumerate(lenses)]
+        print(boxid, lenses, ding)
+        part2 += sum(ding)
     print("part1:", part1)
     print("part2:", part2)
 
