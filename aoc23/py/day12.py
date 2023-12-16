4c3cc0ee7981d074668724d77e3a12c08d3808a3
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-16 19:54:27 +0200

    12/2

--- aoc23/py/day12.py
+++ aoc23/py/day12.py
@@ -65,24 +65,53 @@ def admit(springs, prefix, left):
     missing_ones = sum(prefix) - Counter(springs)['1'] - Counter(left)['1']
     places_for_ones = Counter(left)['?']
 
+    # In a contiguous ?1 block we can definitely not fit more than the amount of ?
+    # as ones.  But if the len of the block is longer than prefix[0] we also need to spend some ? on separating 0.
+    #
+    #
+    # So threshold is in fact  
+
+# 101 1,1,3
+#
+#
+    
+
     # sum(counts) - sum(1s_so_far) = to_place <= len(? left)
     if missing_ones > places_for_ones:
         return 0
     
     subprefix = prefix[:len(subcounts)]
+
+    # On an exact match we can collapse all states into a count if we know the next character is not a ?
     if subcounts == subprefix:
-        return 1
+        return 2
     elif subcounts[:-1] == subprefix[:-1] and subcounts[-1] < subprefix[-1]:
         return 1
     else:
         return 0
 
+
+def prefix_admissable(prefix, full, sofar):
+    subfull = tuple(full[:len(prefix)])
+
+    # If the prefix has length N then there are N - 1 seperators
+    #if sum(prefix) + len(prefix) - 1 > sofar:
+    #    return 0
+
+    if prefix[-1] > subfull[-1]:
+        return 0
+    elif prefix[:-1] == subfull[:-1]:
+        return 1
+    else:
+        return 0
+
+
 def progress(springs, counts):
 
-    states = {""}
+    states = Counter()
+    states[((), "0")] = 1
 
     aanelkaar = "0".join(springs)
-
 
     print(aanelkaar, counts)
     # TODO: instead of string concat this could be bitfields of some kind
@@ -96,33 +125,79 @@ def progress(springs, counts):
     left = aanelkaar
 
     for ix, char in enumerate(aanelkaar):
-        print(springs, ix, len(aanelkaar))
-        nextstates = set()
+        print(ix, "/", len(aanelkaar), aanelkaar[:ix + 1])
+        nextstates = Counter()
         #if states ==  {'00001', '10101', '00101'}:
         #    breakpoint()
-        for state in states:
-            batch = set()
-            if char == "1": 
-                batch.add(state + "1")
-            elif char == "0":
-                batch.add(state + "0")
-            elif char == "?":
-                total_unknown -= 1
-                batch.add(state + "1")
-                batch.add(state + "0")
 
-            nextstates |= {st for st in batch if admit(st, counts, left)}
+        # States ending in zero can not increase their prefix anymore and we can simply count how many there are per prefix.
+        # States ending in one  may branch and there is a difference between
+        #
+        # 1010
+        # 1001 
+        #
+        # as the latter may develop into (1, 2) and the former may not
+#
+#  So our state space is (prefix, last char, count)  
+#  This we can do with a counter on (prefix, last char)
+#
+#
+#
+        # ???0111  1, 1, 3
+        # 0, 1     00: 1,  01: 1     (): 1,  (1,): 1
+        # 00, 01, 10, 11   (), 0: 1,  
+        # 000, 001, 010, 011, 100, 101, 110, 111
 
-        left = left[1:]
-        #print(ix, char, states, "->", [(score(n), n) for n in nextstates])
-        #breakpoint()
+        for state, count in states.items():
+            prefix, lastchar = state
+
+            # (1, 1, 1) -> (1, 1, 2)
+
+            if prefix == ():
+                prefixplus = prefixand = (1,)
+            else:
+                prefixplus = prefix[:-1] + (prefix[-1] + 1,)
+                prefixand = prefix + (1,)
+
+            batch = Counter()
+
+            match (lastchar, char):
+                case "1", "1": # We extend a contiguous string of 1s with one more
+                    if prefix_admissable(prefixplus, counts, ix + 1):
+                        batch[(prefixplus, char)] += count
+                case "0", "1": # Another contiguous batch starts
+                    if prefix_admissable(prefixand, counts, ix + 1):
+                        batch[(prefixand, char)] += count
+                    
+                case _, "0": # we continue, prefix remains admissable
+                    batch[(prefix, "0")] += count
+                case "1", "?": # We split
+                    # Adding a terminating zero carries on the prefix
+                    batch[(prefix, "0")] += count
+
+                    # Adding a 1 either extends
+                    batch[(prefixplus, "1")] += count
+                case "0", "?": # We split
+                    batch[(prefix, "0")] += count
+                    # or lengthens
+                    #breakpoint()
+                    #if aanelkaar[:ix] == '?111????????':
+                    #    breakpoint()
+                    if prefix_admissable(prefixand, counts, ix + 1):
+                        batch[(prefixand, "1")] += count
+
+            nextstates += batch
+
+        for pp in sorted(nextstates):
+            print("    ", pp, nextstates[pp])
+        #print("   ", sorted(states.items()), "->", sorted(nextstates.items()))
         states = nextstates
 
-    trim_states = {n for n in nextstates if score(n) == counts}
-    print(springs, len(nextstates), len(trim_states))
+    print(states)
     #print(aanelkaar, counts)
     #breakpoint()
-    return len(trim_states)
+
+    return sum(count for ((prefix, lastchar), count) in states.items() if prefix == counts)
 
 
 def day12(filename):
@@ -150,7 +225,7 @@ def day12(filename):
             springs = "?".join(5 * [springs]).replace("#", "1")
             counts = ",".join(5 * [counts])
             counts = counts.split(",")
-            kounts = list(map(int, counts))
+            kounts = tuple(map(int, counts))
             expanded = "0".join("1" * int(leng) for leng in counts) 
 
             tot = Counter(expanded)
