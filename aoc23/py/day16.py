14fcbfca09202c51427f9d47f03a18b0b70a14cd
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-16 07:22:05 +0200

    maybe

---
+++ aoc23/py/day16.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+from collections import Counter, defaultdict, deque
+from utils import examples, inputs, display_dfield
+
+import pandas as pd
+import scipy
+import numpy as np
+import networkx as nx
+import itertools
+import math
+
+
+def day16(filename):
+    print()
+    print(filename)
+
+    part1 = 0
+    part2 = 0
+
+    data = []
+
+    with open(filename) as puzzlein:
+        for line in puzzlein:
+            data.append(list(line.strip()))
+
+    df = pd.DataFrame(data)
+
+
+    heads = [(0, 0)]
+    directions = [(0, 1)]
+    seen = set()
+
+    energized = pd.DataFrame(data=np.nan, index=df.index, columns=df.columns)
+    energized[heads[0][1]][heads[0][0]] = '#'
+
+    while True:
+        nexthd = list(zip(heads, directions))
+        heads = []
+        directions = []
+
+        nextseen = set(nexthd)
+        if nextseen.issubset(seen):
+            break
+
+        seen |= nextseen 
+        for (h, d) in nexthd:
+            advance = h[0] + d[0], h[1] + d[1]
+            seen.add((h, d))
+            if advance[0] in df.index and advance[1] in df.columns:
+                row, col = advance
+                char = df[col][row]
+                energized[col][row] = '#'
+
+                if char == '.':
+                    heads.append(advance)
+                    directions.append(d)
+                elif char == '|':
+                    if d in ((0, -1), (0, 1)):
+                        heads.append(advance)
+                        heads.append(advance)
+                        directions.extend([(1, 0), (-1, 0)])
+                    else:
+                        heads.append(advance)
+                        directions.append(d)
+                elif char == '-':
+                    if d in ((-1, 0), (1, 0)):
+                        heads.append(advance)
+                        heads.append(advance)
+                        directions.extend([(0, -1), (0, 1)])
+                    else:
+                        heads.append(advance)
+                        directions.append(d)
+                elif char == '/':
+                    if d == (-1, 0):
+                        heads.append(advance)
+                        directions.append((0, 1))
+                    elif d == (1, 0):
+                        heads.append(advance)
+                        directions.append((0, -1))
+                    elif d == (0, -1):
+                        heads.append(advance)
+                        directions.append((1, 0))
+                    elif d == (0, 1):
+                        heads.append(advance)
+                        directions.append((-1, 0))
+                elif char == '\\':
+                    if d == (-1, 0):
+                        heads.append(advance)
+                        directions.append((0, -1))
+                    elif d == (1, 0):
+                        heads.append(advance)
+                        directions.append((0, 1))
+                    elif d == (0, -1):
+                        heads.append(advance)
+                        directions.append((-1, 0))
+                    elif d == (0, 1):
+                        heads.append(advance)
+                        directions.append((1, 0))
+
+
+    print(energized)
+    part1 =  (energized == '#').sum().sum()
+    print("part1:", part1)
+    print("part2:", part2)
+    breakpoint()
+
+
+day16(examples("16"))
+day16(inputs("16"))
