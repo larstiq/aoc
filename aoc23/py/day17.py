da0ac124582e2eb0bbd742f624c37d4c4e6be3fd
Wouter van Heijst <wouter.vanheijst@wartsila.com>
2023-12-17 10:53:08 +0200

    works

--- aoc23/py/day17.py
+++ aoc23/py/day17.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python
 
-from collections import Counter, defaultdict, deque
+from collections import Counter
 from utils import examples, inputs, display_dfield
 
 import pandas as pd
-import scipy
-import numpy as np
-import networkx as nx
-import itertools
-import math
 
 from heapq import heappush, heappop
-
-from dataclasses import dataclass, field
-from typing import Any
-
-@dataclass(order=True)
-class State:
-    loss: int
-
 
 def day17(filename):
     print()
@@ -40,18 +27,11 @@ def day17(filename):
     stop = df.shape[0] - 1, df.shape[1] - 1
 
     UP, DOWN, LEFT, RIGHT = (-1, 0), (1, 0), (0, -1), (0, 1)
-    # More than needed but they will be pruned, so just eliminate
-    # a chance of me getting the naming wrong
-
     states = Counter()
     states[start, DOWN] = 0
     states[start, RIGHT] = 0
-    states[start, UP] = 0
-    states[start, LEFT] = 0
 
     heads = [(loss, state[0], state[1]) for (state, loss) in states.items()]
-
-    path = pd.DataFrame(data=np.nan, index=df.index, columns=df.columns, dtype=object)
 
     TURNS = {
             UP: [LEFT, RIGHT],
@@ -62,21 +42,28 @@ def day17(filename):
     DEBUG = False
 
     while len(heads) > 0:
-        # We can take at most 3 steps, let's add them all so we don't need
-        # to think in the next step but just take the turns
+        # Avoid tracking amount of steps by adding all possible reached squares
+        # after we turn
         
         state = heappop(heads)
-        print(state)
         state_loss, pos, direction = state
         additional_loss = 0
+
+        # Ultracrucible
         for ix in range(1, 11):
             npos = pos[0] + ix*direction[0], pos[1] + ix*direction[1]
+
+            # Since we're casting into the same direction, if we're out of
+            # bends after step N we'll be more out of bounds at step N+1,
+            # terminate the entire ray early.
             if npos[0] not in df.index or npos[1] not in df.columns:
-                continue
+                break
 
             additional_loss += df[npos[1]][npos[0]]
             loss = state_loss + additional_loss
 
+            # Ultracrucible can not turn before step 4, but accumulated losses
+            # should be tracked
             if ix < 4:
                 continue
 
@@ -87,36 +74,9 @@ def day17(filename):
                 heappush(heads, (loss, npos, turn))
                 states[npos, turn] = loss
 
-                if DEBUG:
-                    if direction == RIGHT:
-                        angle = '>'
-                    elif direction == UP:
-                        angle = '^'
-                    elif direction == LEFT:
-                        angle = '<'
-                    elif direction == DOWN:
-                        angle = 'v'
-
-                    path[npos[1]][npos[0]] = angle
-
-        if DEBUG:
-            display_dfield(path.combine_first(stringdf))
-            endstates = {state for state in states if state[0] == stop }
-            if len(endstates) > 0:
-                print(min(states[s] for s in endstates)) 
-
-            curlosses = pd.DataFrame(data=np.nan, index=df.index, columns=df.columns)
-            for state in states:
-                there = curlosses[state[0][1]][state[0][0]]
-                curlosses[state[0][1]][state[0][0]] = np.nanmin([there, states[state]])
-
-            display_dfield(curlosses.combine_first(df))
-            breakpoint()
-
     endstates = {state for state in states if state[0] == stop }
     part1 = min(states[s] for s in endstates)
 
-    print("Too high?", part1 >= 860)
     print("part1:", part1)
     print("part2:", part2)
     breakpoint()
